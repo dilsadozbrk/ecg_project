5e4d723578c1a9b7f98a3619b1876de294c040a9
Dilsad Ozuberk <dilsad@Dilsads-MacBook-Air.local>
2023-03-04 22:25:20 +0100

    Add existing project files to Git

---
+++ data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import glob 
+from scipy.io import loadmat as sc # library to open .mab files
+
+parent_path = ('a-large-scale-12-lead-electrocardiogram-database-for-arrhythmia-study-1.0.0/WFDBRecords/01/010/')
+paths_hea = glob.glob(parent_path + '*.hea') #getting path for each .hea file
+paths_mab = glob.glob(parent_path + '*.mat') #getting path for each .hea file
+print(len(paths_mab))
+
+
+#converting all .mat files to csv and store in ./data/matlab_files directory
+for file in paths_mab:
+    for n in range(1, len(paths_mab)):
+        mat=sc(file)
+        df_mat= pd.DataFrame(mat['val'])
+        df_mat.to_csv(f'data/matlab_files/mab{n}.csv')
+
+lst = []
+for file in paths_hea:
+    with open(file, 'r') as fp:
+        lines = fp.readlines()[13:16] #fetching relavant lines
+    lst.append(lines)                 #nested list with all relavent lines
+
+for i in range(len(lst)):
+    lst[i] = [x for xs in lst[i] for x in xs.split(':')] # spliting list elements to get column values
+    lst[i] = [x.replace("#", "").replace(" ", "").replace("\n", "") for x in lst[i]] # cleaning special characters
+    i +=1
+
+# creating a list to store only column names
+# keys = ['Age', 'Sex', 'Dx']     
+keys = lst[::2]
+# Remove column names from list to store only values
+values = [[sub_itm for sub_itm in sub_list if sub_itm not in keys] for sub_list in lst]  
+df = pd.DataFrame(values, columns = keys) 
+df.to_csv('data/data.csv', sep=';', index=None)
+
+
+
+
+
+
