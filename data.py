f2b023633db5aa35d4f89df6a17a58ab61e910fa
Dilsad Ozuberk <dilsad@Dilsads-MacBook-Air.local>
2023-03-05 00:39:12 +0100

    data.csv uploaded on snowflake

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import pandas as pd
 import glob 
 from scipy.io import loadmat as sc # library to open .mab files
 
-parent_path = ('a-large-scale-12-lead-electrocardiogram-database-for-arrhythmia-study-1.0.0/WFDBRecords/01/010/')
+parent_path = ('raw_data/WFDBRecords/01/010/')
 paths_hea = glob.glob(parent_path + '*.hea') #getting path for each .hea file
 paths_mab = glob.glob(parent_path + '*.mat') #getting path for each .hea file
-print(len(paths_mab))
+print(len(paths_hea))
 
 
 #converting all .mat files to csv and store in ./data/matlab_files directory
@@ -25,13 +25,13 @@ for i in range(len(lst)):
     lst[i] = [x for xs in lst[i] for x in xs.split(':')] # spliting list elements to get column values
     lst[i] = [x.replace("#", "").replace(" ", "").replace("\n", "") for x in lst[i]] # cleaning special characters
     i +=1
-
+print(lst)
 # creating a list to store only column names
-# keys = ['Age', 'Sex', 'Dx']     
-keys = lst[::2]
+keys = ['Age', 'Sex', 'Dx']     
 # Remove column names from list to store only values
 values = [[sub_itm for sub_itm in sub_list if sub_itm not in keys] for sub_list in lst]  
 df = pd.DataFrame(values, columns = keys) 
+#print(df.head(20))
 df.to_csv('data/data.csv', sep=';', index=None)
 
 
