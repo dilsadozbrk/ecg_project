5f9016a78878e713066472294b6eef86841ebcf3
Dilsad Ozuberk <dilsad@Dilsads-MacBook-Air.local>
2023-03-14 13:27:15 +0100

    code after black formatting

--- matlab_files_clean.py
+++ matlab_files_clean.py
@@ -1,31 +1,27 @@
 import pandas as pd
-import glob 
-from scipy.io import loadmat as sc # library to open .mab files
+import glob
+from scipy.io import loadmat as sc  # library to open .mab files
 
 
-paths_mab = sorted(glob.glob('raw_data/*/*/*/*.mat'))
+paths_mab = sorted(glob.glob("raw_data/*/*/*/*.mat"))
+
 
 def cleanup_matmab(files, output):
-
     for n in range(len(files)):
-        patient_id = files[n].split('/')[-1][2:-4]
-        #print('Working on ' + patient_id)
-        mat=sc(files[n])
-        df = pd.DataFrame(mat['val']).T
-        df['Patient_id'] = patient_id
+        patient_id = files[n].split("/")[-1][2:-4]
+        # print('Working on ' + patient_id)
+        mat = sc(files[n])
+        df = pd.DataFrame(mat["val"]).T
+        df["Patient_id"] = patient_id
         if n == 0:
-            df = df.rename(columns = lambda x: x if x == 'Patient_id' else f"Dimension_{int(x)+1}")
-            df.to_csv(output, index = False)
+            df = df.rename(
+                columns=lambda x: x if x == "Patient_id" else f"Dimension_{int(x)+1}"
+            )
+            df.to_csv(output, index=False)
         else:
-            df.to_csv(output, mode= 'a', index = False, header = False)
+            df.to_csv(output, mode="a", index=False, header=False)
+
 
 files = paths_mab
-output = 'data/matlab_files/mab_all.csv'
+output = "data/matlab_files/mab_all.csv"
 cleanup_matmab(files, output)
-
-
-
-
-
-
-
--- run.py
+++ run.py
@@ -3,20 +3,20 @@ import glob
 import multi_def
 import pandas as pd
 
-paths_mab = glob.glob('raw_data/*/*/*/*.mat') 
+paths_mab = glob.glob("raw_data/*/*/*/*.mat")
 file_list = sorted(paths_mab)[:100]
 print(file_list)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         p = Pool()
         p.map(multi_def.cleanup, file_list)
-    
+
     except:
         FileNotFoundError
 
 
-df = pd.read_csv('data/matlab_files/mab_pool.csv')
+df = pd.read_csv("data/matlab_files/mab_pool.csv")
 print(df.shape)
-print(df['Patient_id'].value_counts())
+print(df["Patient_id"].value_counts())
